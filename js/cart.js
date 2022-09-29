adda451ab514e77391b0ce08e23f0a7181148943
Trace Dugar <tracedugar89@gmail.com>
2022-09-29 15:17:47 -0700

    table work

--- js/cart.js
+++ js/cart.js
@@ -19,19 +19,40 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
+ let tableElem = document.querySelector('tbody');
+
 
   // TODO: Iterate over the items in the cart
+  for (let i = 0; i < cart.items.length; i++) {
+    let storeElem = document.createElement('tr');
+    tableElem.appendChild(storeElem);
+    let td1 = document.createElement('td');
+    td1.textContent = 'x';
+    storeElem.appendChild(td1);
+  }
+
   // TODO: Create a TR
+  // elements that are unique per store
+  let row1 = document.createElement('tr');
+  tableElem.appendChild(row1);
+
+  let items = document.createElement('td')
+  location.textContent = this.product;
+  row1.appendChild(items);
+
+};
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
-}
+
 
 function removeItemFromCart(event) {
 
